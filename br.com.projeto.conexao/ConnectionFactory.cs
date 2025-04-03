3f54f80e008d3e572f2958cd4893d4c592a494db
lucasmaurodev <lucasmauro.senac@gmail.com>
2025-04-02 21:21:47 -0300

    Adicionar arquivos de projeto.

---
+++ br.com.projeto.conexao/ConnectionFactory.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bdVendas.br.com.projeto.conexao
+{
+    public class ConnectionFactory
+    {
+        public MySqlConnection getconnection() // É criado um método com retorno MysqlConnection
+        {
+            string conexao = ConfigurationManager.ConnectionStrings["bdvendas"].ConnectionString;
+
+            return new MySqlConnection(conexao);
+        }
+    }
+}
